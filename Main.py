6d7bc4db251cd5653b4b7a53b684582ad8d389c8
chinook82 <cheguevaros@mail.ru>
2017-07-24 15:14:43 +0600

    added new Main.py file for main function and raplace functions from calcMatrix.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+""" main function of numero calculation of matrix by python """
+import sys
+
+
+
+def main(argv):
+
+
+
+    pass
+# -def main-
+
+if __name__ == '__main__':
+    main(sys.argv)
--- calcMatrix.py
+++ calcMatrix.py
@@ -1,10 +1,2 @@
-""" main function of numero calculation of matrix by py """
-
-import sys
-
-def main(argv):
+def calcMatrix():
     pass
-# def main end
-
-if __name__ == '__main__':
-    main(sys.argv)
